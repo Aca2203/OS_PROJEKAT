461346b4b6d453c5d146157b5f5bfad5cf871d75
Aleksandar Stefanovic <sa220196d@student.etf.bg.ac.rs>
2024-09-14 11:20:37 -0700

    Kreiranje fajlova workers.hpp i workers.cpp

---
+++ h/workers.hpp
@@ -0,0 +1,12 @@
+#ifndef WORKERS_H
+#define WORKERS_H
+
+extern void workerBodyA(void*);
+
+extern void workerBodyB(void*);
+
+extern void workerBodyC(void*);
+
+extern void workerBodyD(void*);
+
+#endif
